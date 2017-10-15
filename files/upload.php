94f8bdc3e0feb731b894d83240c4342055bddcb9
Hong <choi.29@wright.edu>
2017-10-15 15:46:58 -0400

    file upload
    file upload function.

---
+++ files/upload.php
@@ -0,0 +1,6 @@
+<?php
+$source = $_FILES['profile']['tmp_name'];
+$dest = "./".basename($_FILES['profile']['name']);
+move_uploaded_file($source, $dest);
+?>
+
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Food_Reviewer</title>
+    </head>
+    <body>
+        <form action="http://localhost/CEG4110_Project/files/upload.php" method="post" enctype="multipart/form-data">
+            <input type="text" name="id">
+            <input type="file" name="profile">
+            <input type="submit">
+        </form>
+        
+    </body>
+</html>
